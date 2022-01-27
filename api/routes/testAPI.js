33d83d2d4c5bd057ba949443bbe65c2c200b3381
Matthew Vandergrift <matthew.vandergrift@ibm.com>
2022-01-27 16:00:29 -0500

    added other code

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,24 @@
 var express = require("express");
 var router = express.Router();
+var axios = require("axios")
 
 router.get("/", function(req, res, next) {
-    res.send("API is working properly");
+    let users = [];
+    try {
+        
+        await axios.default.get('https://jsonplaceholder.typicode.com/users').then(result => {  
+            console.log(result.data);
+            users = result.data.map(res => {
+                users.push(res.username);
+            })
+        })
+        
+    } catch (e) {
+        // def do something
+    }
+    res.send(users);
+    console.log('a')
+    //res.send("API is working properly");
 });
 
 module.exports = router;
---
+++ api/tests/routes/test1.tests.js
@@ -0,0 +1,8 @@
+import {router} from '../routes/testAPI';
+
+describe('testAPI', () => {
+    it('We should get back particular wording', async () => {
+
+        let rtr = new router()
+        });
+        )
